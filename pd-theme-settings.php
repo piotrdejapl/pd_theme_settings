f08fdb6e6d4c17faaa4ff3abbde25147ac49a9da
wronait <piotrdeja.pl@gmail.com>
2021-10-08 13:40:45 +0200

    first commit

---
+++ pd-theme-settings.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+*
+*   Plugin Name: Ustawienia Motywu
+*   Description: Plugin umożliwia kontrolę nad skryptami i funkcjonalnościami które mają się ładować na stronie. Oprócz tego daje możliwość dodawania własnych custom post types. 
+*/
+
+
+
+/**
+ * Get the bootstrap! If using the plugin from wordpress.org, REMOVE THIS!
+ */
+
+if ( file_exists( dirname( __FILE__ ) . '/cmb2/init.php' ) ) {
+    require_once dirname( __FILE__ ) . '/cmb2/init.php';
+} elseif ( file_exists( dirname( __FILE__ ) . '/CMB2/init.php' ) ) {
+    require_once dirname( __FILE__ ) . '/CMB2/init.php';
+}
+
+
+
+
+
+add_action( 'cmb2_admin_init', 'cmb2_sample_metaboxes', 10);
+
+
+
+
+require_once(plugin_dir_path(__FILE__) . './libs/admin-view.php');
+
+
+
